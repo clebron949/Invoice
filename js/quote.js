f54a71a69fec4f6de4462bf7a78ab96b4289dc0a
Christopher Lebron <christopher.lebron1@gmail.com>
2022-08-21 18:45:28 -0400

    added new index and template

---
+++ js/quote.js
@@ -0,0 +1,23 @@
+class ClientData {
+    constructor(){
+        this.clientName;
+        this.clientPhoneNumber;
+        this.clientAddress
+        this.clientCity
+        this.clientZipCode
+    }
+}
+
+class QuoteData{
+    constructor(){
+        this.serviceType;
+        this.description;
+        this.price;
+        this.quantity;
+        this.amount
+    }
+}
+
+function GetQuote(){
+        
+};
